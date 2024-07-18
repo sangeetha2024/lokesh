2d627c240e63399540735c982c305dfebfe035ee
naveenkumar <navee@6578>
2024-07-18 17:03:37 +0530

    fdg

--- lokesh.py
+++ lokesh.py
@@ -1,1 +1,77 @@
-print("naveen")
+# variable
+"""
+Aa=10
+10A=10
+_ab10=10
+b=10.3
+
+c=3+4j
+
+name = "sangeetha"
+
+name1= 'sangee'
+
+name2= 's'
+
+# list
+
+a=[] # emptylist
+
+a=['s','v',"sangetha",12,3.3,3+5j]
+
+#tuple
+
+a=('jg',98,6.6)
+
+
+#dict
+
+a={
+    "a":"sanggetha"
+}
+
+
+#set
+
+a={3,3,4,6,7,"jhg"}
+
+
+
+
+# module signup
+
+
+jio_username= "sangeetha"
+jio_userphono=9087654321
+
+print(jio_username)
+print(jio_userphono)
+
+
+#user input
+
+user_name=input("Enter your name")
+user_mobile=int(input("Enter the phone number:"))
+print(type(user_name))
+print(type(user_mobile))
+#class <str>
+#class <int>
+
+a=input()
+print(a)
+
+"""
+user_firstvalue=int(input("enter the first number"))
+user_secondvalue=int(input("enterthe second value"))
+addition=user_firstvalue+user_secondvalue
+subraction=user_firstvalue-user_secondvalue
+multiplication=user_firstvalue*user_secondvalue
+division=user_firstvalue/user_secondvalue
+modules=user_firstvalue%user_secondvalue
+exponent=user_firstvalue**user_secondvalue
+print(addition)
+print(subraction)
+print(multiplication)
+print(division)
+print(modules)
+print(exponent)                        
