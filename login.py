4e20ed0a7389951c69d20fc0f3f7e5b95773106f
naveenkumar <navee@6578>
2024-07-19 15:50:59 +0530

    added

---
+++ login.py
@@ -0,0 +1,9 @@
+user='admin'
+username=input("ENTER THE USER NAME")
+if user==username:
+    print("valid user")
+elif user!=username:
+    print("you are not valid user")
+else:
+    print("ENTER THE VALID NAME ")
+    
