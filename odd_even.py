91d77ac2a8294f1f28d00e817feddb0ae582ca88
naveenkumar <navee@6578>
2024-07-19 15:43:29 +0530

    bye

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+User_value=int(input("Enter the Number"))
+if User_value % 2==0:
+    print("given number is even",User_value)
+elif User_value != 0:
+    print("given number is odd")
+else:
+    print("Enter the crt number")
