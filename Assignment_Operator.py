6199ad47d1ab4017f712bfcc111b521824b634a1
naveenkumar <navee@6578>
2024-07-19 15:08:10 +0530

    fhhg

---
+++ Assignment_Operator.py
@@ -0,0 +1,69 @@
+user1=int(input("Enter the number 1"))
+user2=int(input("Enter the number 2"))
+
+#Assignment Operator
+cal1=user1
+cal2=user2
+
+
+# (+)Assignment Operator
+
+#method1) x=x+1
+#method2) x+=1
+
+x=0
+x=x+1
+print("method1",x)
+x=12
+x+=1
+print("method2",x)
+
+# (-)Assignment Operator
+
+#method1) x=x-1
+#method2) x-=1
+
+x=0
+x=x-1
+print("method1",x)
+x=12
+x-=1
+print("method2",x)
+
+
+
+# (*)Assignment Operator
+
+#method1) x=x*1
+#method2) x*=1
+
+x=0
+x=x*1
+print("method1",x)
+x=12
+x*=1
+print("method2",x)
+
+
+# (/)Assignment Operator
+
+#method1) x=x/1 
+#method2) x//=1
+
+x=12
+x=x/1
+print("method1",x)
+x=12
+x//=1
+print("method2",x)
+
+
+# (**)Assignment Operator
+
+#method1) x=x/1 
+#method2) x//=1
+
+
+x=12
+x**=1
+print("method2",x)
