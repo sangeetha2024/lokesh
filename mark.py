cd70a92343123574251811c8ea5af84d8effef91
naveenkumar <navee@6578>
2024-07-19 16:07:59 +0530

    ygshdgh

---
+++ mark.py
@@ -0,0 +1,12 @@
+stu_per=int(input("enter your percentage"))
+if stu_per<=100 and stu_per>=90:
+
+    print("a grade")
+elif stu_per<=89 and stu_per>=60:
+    print("b grade")
+elif stu_per<=59 and stu_per>=35:
+    print("c grade")
+ 
+
+else:
+    print("sethu po")   
